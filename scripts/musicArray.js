7a11a699e70d60b10d53fd24949a384d5aa170f5
Mihail <manchenko2016@list.ru>
2021-04-29 23:42:57 +0300

    first commit

---
+++ scripts/musicArray.js
@@ -0,0 +1,14 @@
+let audioArray = [
+  {
+    'music': 'audio/audio1.mp3'
+  },
+  {
+    'music': 'audio/audio2.mp3'
+  },
+  {
+    'music': 'audio/audio3.mp3'
+  },
+  {
+    'music': 'audio/audio1.mp3'
+  }
+]
